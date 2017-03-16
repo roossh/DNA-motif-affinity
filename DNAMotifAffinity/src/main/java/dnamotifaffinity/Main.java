704433359c638e11c55f5ee38052187815c9562d
roossh <roosa-maria.plaketti@helsinki.fi>
2017-03-16 18:12:52 +0200

    testiluokkien muistiinpalauttamista ja pitin testaus...

---
+++ DNAMotifAffinity/src/main/java/dnamotifaffinity/Main.java
@@ -0,0 +1,12 @@
+package dnamotifaffinity;
+
+public class Main {
+    public static void main(String[] args) {
+        //printHello();
+        Testiluokka testi = new Testiluokka();
+        System.out.println(testi.sayHello());
+    }
+    public static void printHello() {
+        System.out.println("hello");
+    }
+}
---
+++ DNAMotifAffinity/src/main/java/dnamotifaffinity/Testiluokka.java
@@ -0,0 +1,14 @@
+
+package dnamotifaffinity;
+
+public class Testiluokka {
+    private String vastaus = "hello";
+    
+    public Testiluokka() {
+        this.vastaus = vastaus;
+    }
+    
+    public String sayHello() {
+        return this.vastaus;
+    }
+}
